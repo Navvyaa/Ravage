23104f9793e9f18d4fb2a8e5a2bc5c4c0f74384a
Ritika <ritikatiwari784@gmail.com>
2024-10-12 15:26:59 +0530

    created group of persons, moving in the map

--- game/script.js
+++ game/script.js
@@ -4,9 +4,11 @@ const mapImage = new Image();
 mapImage.src = 'assets/map.jpg';
 const characterImage = new Image();
 characterImage.src = 'assets/character.png';
+const movingCharacterImage = new Image();
+movingCharacterImage.src = 'assets/person.png';
 
-canvasWidth=canvas.width = window.innerWidth;
-canvasHeight=canvas.height = window.innerHeight;
+canvasWidth = canvas.width = window.innerWidth;
+canvasHeight = canvas.height = window.innerHeight;
 
 let angle = 0;
 let position = { x: 500, y: 0 };
@@ -40,33 +42,81 @@ class Bullet {
 let bullets = [];
 
 const restrictedAreas = [
-    { x1: 909, y1: 866, x2: 1290, y2:1972 },
+    { x1: 909, y1: 866, x2: 1290, y2: 1972 },
     { x1: 2569, y1: 2601, x2: 2913, y2: 2849 }
 ];
 
 function isWithinRestrictedArea(newX, newY) {
     for (const area of restrictedAreas) {
-        if (
-            newX > area.x1 &&
-            newX < area.x2 &&
-            newY > area.y1 &&
-            newY < area.y2
-        ) {
+        if (newX > area.x1 && newX < area.x2 && newY > area.y1 && newY < area.y2) {
             return true;
         }
     }
     return false;
 }
 
+class MovingCharacter {
+    constructor(x, y, direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+        this.size = 30;
+        this.speed = 0.5;
+    }
+
+    move() {
+        this.x += Math.cos(this.direction) * this.speed;
+        this.y += Math.sin(this.direction) * this.speed;
+
+        if (this.x < 0 || this.x > mapImage.width || this.y < 0 || this.y > mapImage.height) {
+            this.direction += Math.PI;
+        }
+    }
+
+    checkCollision(other) {
+        const dx = other.x - this.x;
+        const dy = other.y - this.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        return distance < this.size;
+    }
+
+    reverseDirection() {
+        this.direction += Math.PI;
+    }
+
+    draw() {
+        ctx.drawImage(movingCharacterImage, this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
+    }
+}
+
+let movingCharacters = [];
+for (let i = 0; i < 20; i++) {
+    const x = Math.random() * canvasWidth;
+    const y = Math.random() * canvasHeight;
+    const direction = Math.random() * 2 * Math.PI;
+    movingCharacters.push(new MovingCharacter(x, y, direction));
+}
+
+function handleCollisions() {
+    for (let i = 0; i < movingCharacters.length; i++) {
+        for (let j = i + 1; j < movingCharacters.length; j++) {
+            if (movingCharacters[i].checkCollision(movingCharacters[j])) {
+                movingCharacters[i].reverseDirection();
+                movingCharacters[j].reverseDirection();
+            }
+        }
+    }
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+
     ctx.save();
     ctx.scale(zoomLevel, zoomLevel);
-    
+
     const mapX = Math.max(Math.min(-position.x + canvasWidth / (2 * zoomLevel), 0), -(mapImage.width - canvasWidth / zoomLevel));
     const mapY = Math.max(Math.min(-position.y + canvasHeight / (2 * zoomLevel), 0), -(mapImage.height - canvasHeight / zoomLevel));
-    
+
     ctx.drawImage(mapImage, mapX, mapY);
     ctx.restore();
 
@@ -82,6 +132,8 @@ function draw() {
     bullets.forEach(bullet => {
         bullet.draw();
     });
+
+    movingCharacters.forEach(character => character.draw());
 }
 
 function update() {
@@ -113,7 +165,7 @@ function update() {
         position.x = newX;
         position.y = newY;
     } else {
-        console.log("Restricted area, can't move here!");
+        console.log("Restricted area,can not move here!!!!");
     }
 
     bullets.forEach((bullet, index) => {
@@ -122,6 +174,10 @@ function update() {
             bullets.splice(index, 1);
         }
     });
+
+
+    movingCharacters.forEach(character => character.move());
+    handleCollisions();
 }
 
 document.addEventListener('keydown', (event) => {
@@ -154,7 +210,7 @@ function gameLoop() {
 }
 
 let imagesLoaded = 0;
-const totalImages = 2;
+const totalImages = 3;
 
 mapImage.onload = () => {
     imagesLoaded++;
@@ -164,6 +220,13 @@ mapImage.onload = () => {
 };
 
 characterImage.onload = () => {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        gameLoop();
+    }
+};
+
+movingCharacterImage.onload = () => {
     imagesLoaded++;
     if (imagesLoaded === totalImages) {
         gameLoop();
